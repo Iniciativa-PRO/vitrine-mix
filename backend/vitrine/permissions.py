bde810170e372ed5faaff91efd33f7a6ce4d1d34
Adriano Romero <adriano.romerofco@gmail.com>
2023-03-19 15:05:49 -0300

    fat(backend): Adicionado permissões para StoreFront e Services

---
+++ backend/vitrine/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+
+class IsOwnerStoreOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.creator == request.user
+
+
+class IsOwnerServiceOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.store.creator == request.user
