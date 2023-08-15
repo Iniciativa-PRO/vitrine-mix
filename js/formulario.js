e1825eafbc6a18b4bd59e8f03b70a0914ec03e90
jose <lenadrojose124leandro@gmail.com>
2023-08-15 08:24:22 -0300

    Criação da página de perfil - com os arquivos HTML e CSS relacionados a criação e um JavaScript para a remoção do comportamento padrão do botão continuar e redirecionamento para a está nova página

---
+++ js/formulario.js
@@ -0,0 +1,6 @@
+let btnForm = document.querySelector(".btn_form");
+
+btnForm.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href ="pagina-perfil-vitrines.html"
+})
